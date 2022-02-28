a9591f71f8ae418c2b725cd994a8168a91b83ead
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-02-27 23:29:27 -0600

    final commit

--- app.js
+++ app.js
@@ -2,10 +2,19 @@
 // Nondestructively sort the letters in the city's name.
 
 const allCap = (city) => {
-    let capCity = city.slice()
-    let sortCity = capCity[0].toUpperCase() + capCity.slice(1)
-    return sortCity.split('').sort((a,b) => a.toLowerCase().localeCompare(b.toLowerCase())).join('')
-  
+    // let capCity = city.slice()
+    // let sortCity = capCity[0].toUpperCase() + capCity.slice(1)
+    // console.log(city)
+    // return sortCity.split('').sort((a,b) => a.toLowerCase().localeCompare(b.toLowerCase())).join('')
+
+    // If you can find a better, much shorter solution then mine above
+    // code below
+
+
+
+    
 }
 
-module.exports = allCap
+module.exports = allCap 
+
+
--- app.test.js
+++ app.test.js
@@ -2,5 +2,5 @@ const allCap = require('./app')
 
 test("should be capitalized and letters in alphabetical order", () => {
     const city = 'jerusalem'
-    expect(allCap(city)).toMatch(/aeeJlmrsu/)
+    expect(allCap(city)).toBe('aeeJlmrsu')
 })
