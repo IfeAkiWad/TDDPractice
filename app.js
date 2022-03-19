2e902f01d74e416855a36207df9a69fcdaab987b
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-03-19 14:58:46 -0500

    initial commit

--- app.js
+++ app.js
@@ -1,20 +1,2 @@
-// Nondestructively capitalize the first letter of the city's name.
-// Nondestructively sort the letters in the city's name.
-
-const allCap = (city) => {
-    // let capCity = city.slice()
-    // let sortCity = capCity[0].toUpperCase() + capCity.slice(1)
-    // console.log(city)
-    // return sortCity.split('').sort((a,b) => a.toLowerCase().localeCompare(b.toLowerCase())).join('')
-
-    // If you can find a better, much shorter solution then mine above
-    // code below
-
-
-
-    
-}
-
-module.exports = allCap 
 
 
